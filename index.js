439d4c059d0ab82c4cfd97c336b39c60b0c8753a
Seth Ullman <seth.ullman98@gmail.com>
2021-01-15 23:52:35 -0700

    add create todo

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const TodoTask = require("./models/TodoTask");
 
 mongoose.set("useFindAndModify", false);
 
@@ -21,11 +22,21 @@ app.get('/',(req, res) => {
   
 });
 
-app.post('/',(req, res) => {
+app.post('/', async (req, res) => {
   
-  console.log(req.body);
+  const todoTask = new TodoTask({
+    content: req.body.content
+  });
+
+  try {
+    await todoTask.save();
+    res.redirect("/");
+  } catch (e) {
+    console.log(e);
+    res.redirect("/");
+  }
   
-})
+});
 
 mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => {
   console.log("Connected to db!");
