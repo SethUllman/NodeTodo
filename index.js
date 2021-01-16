42ae446a5a88bde048da3917a2f3e973ffc7578a
Seth Ullman <seth.ullman98@gmail.com>
2021-01-15 23:14:08 -0700

    add post route

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const app = express();
 
 app.use('/static', express.static('public'));
 
+app.use(express.urlencoded({ extended: true }));
+
 app.set("view engine", "ejs");
 
 app.get('/',(req, res) => {
@@ -11,5 +13,11 @@ app.get('/',(req, res) => {
   res.render('todo.ejs')
 
 });
+
+app.post('/',(req, res) => {
+
+  console.log(req.body);
+
+})
 
 app.listen(3000, () => console.log("Server Up and running"));
